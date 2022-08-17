6c9aa1b2edb00aa5fabb83c8168f1dd0df0c1275
addeponken <addeponken@hotmail.com>
2022-08-17 15:22:40 +0200

    initial

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Numerics;
+
+namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(params string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine(DateTime.Now.ToLongTimeString());
+        Console.ReadLine();
+        var v = new Vector2(5);
+    }
+}
